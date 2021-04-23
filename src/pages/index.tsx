eb037355dce8ea8ebe41dde4b517ee5245bc8802
Fabiana Viana <fabiana.viana@reclameaqui.com.br>
2021-04-22 23:37:17 -0300

    feat: created episodes section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,28 +46,104 @@
 //Assim que alguém acessa essa página é gerado uma versão estatica 
 //-Evita gastar recurso de requisição
 //-Página muito mais performática
+import { GetStaticProps } from 'next';
+import Image from  'next/image';
+import { api } from '../services/api';
+import {format, parseISO} from 'date-fns';
+import  ptBR  from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import styles from './home.module.scss';
 
+type Episodes = {
+  id: string,
+  title: string,
+  thumbnail: string,
+  description: string,
+  members: string,
+  duration: number,
+  durationAsString: string,
+  url: string,
+  publishedAT: string,
+}
 
-export default function Home(props) {
-  console.log(props.episodes)
+type HomeProps = {
+  // episodes: Episodes[];
+  //ou episodes: Array<Episodes>
+  latestEpisodes: Episodes[];
+  allEpisodes: Episodes[];
+}
 
+export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
   return (
-    <div>
-    <h1>Index</h1>
+    <div className={styles.homepage}>
+    <section className={styles.latestEpisodes}>
+      <h2>Últimos lançamentos</h2>
+      <ul>
+        {latestEpisodes.map(episodes => {
+          return (
+            <li key={episodes.id}> 
+              <Image 
+                width={192} 
+                height={192} 
+                src={episodes.thumbnail} 
+                alt={episodes.title} 
+                />
+
+              <div className={styles.episodeDetails}>
+                <a href="">{episodes.title}</a>
+                <p>{episodes.members}</p>
+                <span>{episodes.publishedAT}</span>
+                <span>{episodes.durationAsString}</span>
+              </div>
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episódio" />
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+    <section className={styles.allEpisodes}>
+      
+    </section>
     </div>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAT: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length)
 
   return {
     props: {
-      episodes: data,
+      episodes,
+      latestEpisodes,
+      allEpisodes,
     },
-    //a mais no SSG passar o REVALIDATE  recebe um numero em segundos de quanto em quanto tempo preciso gerar um nova chamada na api, dentro do pedíodo eles receberam uma pagina statica
-    //mas o SSG só atende em produção
-    revalidate: 60 * 60 * 0,
+    revalidate: 60 * 60 * 8,
   }
+  //a mais no SSG passar o REVALIDATE  recebe um numero em segundos de quanto em quanto tempo preciso gerar um nova chamada na api, dentro do pedíodo eles receberam uma pagina statica
+    //mas o SSG só atende em produção
 }
